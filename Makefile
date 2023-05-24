9b34b8c33b2007bb57db2d57581dfa2005c71041
pkkn2 <102573730+pkkn2@users.noreply.github.com>
2023-05-24 17:58:19 +0000

    Add make format fix

--- Makefile
+++ Makefile
@@ -9,6 +9,6 @@ lint:
 	pylint --disable=R,C *.py
 
 format:
-    black *.py
+	black *.py
 
 all: install lint test
--- myStatements.py
+++ myStatements.py
@@ -1,5 +1,5 @@
 my_list = []
-my_dict = {"One": 1, "Two":2, "Three":3}
+my_dict = {"One": 1, "Two": 2, "Three": 3}
 
-for k,v in my_dict.items():
+for k, v in my_dict.items():
     print(k, v)
